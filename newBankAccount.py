cbf32eb0ad28ea5512f409b5945869832de3081e
lazyeasteregg <34332730+lazyeasteregg@users.noreply.github.com>
2022-04-23 22:47:41 -0700

    iterative changes to newbankaccount.py file

--- newBankAccount.py
+++ newBankAccount.py
@@ -5,12 +5,21 @@ class Bank:
         self.name = name
         self.accountnum = accountnumber
         self.balance = initialBalance
-        tstamp = strftime("%Y %b %d %H-%M-%S", localtime())
-        self.openlog = {self.name, self.accountnum, self.balance, tstamp}
+        self.txlog = {}
+        self.tstamp = strftime("%Y %b %d %H-%M-%S", localtime())
 
-#    def open_acct(self):
-#       tstamp = strftime("%Y %b %d %H-%M-%S", localtime())
+    def open_acct(self):
+        return "name: {} \t account: {} \t balance: ${} \t date: {}".format(self.name, self.accountnum, self.balance, self.tstamp)
+
+    def tslog(self):
+        tx = tuple([self.balance, self.tstamp])
+        self.txlog[self.accountnum] = {self.name, tx}
+
+    def visitor(self):
+        return "{} {} {}".format(self.name, self.accountnum, self.balance)
+
 
 user1 = Bank("john", 87324, 9831)
-print(user1.openlog)
-#print(user1.tstamp)
+
+#print(user1.open_acct())
+print(user1.tslog())
